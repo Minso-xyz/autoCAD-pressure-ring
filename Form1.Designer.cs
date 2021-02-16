c0cdccb506b5d25d8a2e183d644d256953ce08ef
Minso <65314416+Minso-xyz@users.noreply.github.com>
2021-02-16 21:41:49 +0100

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,346 @@
+﻿namespace TestaUN
+{
+    partial class button_insert
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.radioButton_endless = new System.Windows.Forms.RadioButton();
+            this.radioButton_split = new System.Windows.Forms.RadioButton();
+            this.textBox_intDia = new System.Windows.Forms.TextBox();
+            this.textBox_extDia = new System.Windows.Forms.TextBox();
+            this.textBox_height = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.radioButton_nylon = new System.Windows.Forms.RadioButton();
+            this.radioButton_ptfe = new System.Windows.Forms.RadioButton();
+            this.labelWarning = new System.Windows.Forms.Label();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.comboBoxProfile = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(495, 335);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 49);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "INSERT!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 28);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Int. Diameter : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 69);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Ext. Diameter : ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 109);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Height (Imp.) : ";
+            // 
+            // radioButton_endless
+            // 
+            this.radioButton_endless.AutoSize = true;
+            this.radioButton_endless.Checked = true;
+            this.radioButton_endless.Location = new System.Drawing.Point(20, 28);
+            this.radioButton_endless.Margin = new System.Windows.Forms.Padding(4);
+            this.radioButton_endless.Name = "radioButton_endless";
+            this.radioButton_endless.Size = new System.Drawing.Size(79, 21);
+            this.radioButton_endless.TabIndex = 4;
+            this.radioButton_endless.TabStop = true;
+            this.radioButton_endless.Text = "Endless";
+            this.radioButton_endless.UseVisualStyleBackColor = true;
+            // 
+            // radioButton_split
+            // 
+            this.radioButton_split.AutoSize = true;
+            this.radioButton_split.Location = new System.Drawing.Point(200, 28);
+            this.radioButton_split.Margin = new System.Windows.Forms.Padding(4);
+            this.radioButton_split.Name = "radioButton_split";
+            this.radioButton_split.Size = new System.Drawing.Size(56, 21);
+            this.radioButton_split.TabIndex = 5;
+            this.radioButton_split.Text = "Split";
+            this.radioButton_split.UseVisualStyleBackColor = true;
+            // 
+            // textBox_intDia
+            // 
+            this.textBox_intDia.Location = new System.Drawing.Point(125, 25);
+            this.textBox_intDia.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox_intDia.Name = "textBox_intDia";
+            this.textBox_intDia.Size = new System.Drawing.Size(104, 22);
+            this.textBox_intDia.TabIndex = 6;
+            // 
+            // textBox_extDia
+            // 
+            this.textBox_extDia.Location = new System.Drawing.Point(125, 65);
+            this.textBox_extDia.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox_extDia.Name = "textBox_extDia";
+            this.textBox_extDia.Size = new System.Drawing.Size(104, 22);
+            this.textBox_extDia.TabIndex = 7;
+            // 
+            // textBox_height
+            // 
+            this.textBox_height.Location = new System.Drawing.Point(125, 106);
+            this.textBox_height.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox_height.Name = "textBox_height";
+            this.textBox_height.Size = new System.Drawing.Size(104, 22);
+            this.textBox_height.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(239, 28);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 17);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "[mm]";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(239, 69);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 17);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "[mm]";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(239, 110);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(38, 17);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "[mm]";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.textBox_intDia);
+            this.groupBox1.Controls.Add(this.textBox_height);
+            this.groupBox1.Controls.Add(this.textBox_extDia);
+            this.groupBox1.Location = new System.Drawing.Point(16, 15);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(295, 151);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Dimensions";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioButton_endless);
+            this.groupBox2.Controls.Add(this.radioButton_split);
+            this.groupBox2.Location = new System.Drawing.Point(16, 174);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Size = new System.Drawing.Size(295, 70);
+            this.groupBox2.TabIndex = 13;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Split type";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.radioButton_nylon);
+            this.groupBox3.Controls.Add(this.radioButton_ptfe);
+            this.groupBox3.Location = new System.Drawing.Point(16, 252);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox3.Size = new System.Drawing.Size(295, 70);
+            this.groupBox3.TabIndex = 14;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Material";
+            // 
+            // radioButton_nylon
+            // 
+            this.radioButton_nylon.AutoSize = true;
+            this.radioButton_nylon.Checked = true;
+            this.radioButton_nylon.Location = new System.Drawing.Point(20, 28);
+            this.radioButton_nylon.Margin = new System.Windows.Forms.Padding(4);
+            this.radioButton_nylon.Name = "radioButton_nylon";
+            this.radioButton_nylon.Size = new System.Drawing.Size(65, 21);
+            this.radioButton_nylon.TabIndex = 4;
+            this.radioButton_nylon.TabStop = true;
+            this.radioButton_nylon.Text = "Nylon";
+            this.radioButton_nylon.UseVisualStyleBackColor = true;
+            // 
+            // radioButton_ptfe
+            // 
+            this.radioButton_ptfe.AutoSize = true;
+            this.radioButton_ptfe.Location = new System.Drawing.Point(200, 28);
+            this.radioButton_ptfe.Margin = new System.Windows.Forms.Padding(4);
+            this.radioButton_ptfe.Name = "radioButton_ptfe";
+            this.radioButton_ptfe.Size = new System.Drawing.Size(64, 21);
+            this.radioButton_ptfe.TabIndex = 5;
+            this.radioButton_ptfe.Text = "PTFE";
+            this.radioButton_ptfe.UseVisualStyleBackColor = true;
+            // 
+            // labelWarning
+            // 
+            this.labelWarning.AutoSize = true;
+            this.labelWarning.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelWarning.ForeColor = System.Drawing.Color.Red;
+            this.labelWarning.Location = new System.Drawing.Point(16, 335);
+            this.labelWarning.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelWarning.Name = "labelWarning";
+            this.labelWarning.Size = new System.Drawing.Size(0, 15);
+            this.labelWarning.TabIndex = 12;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.pictureBox1);
+            this.groupBox4.Controls.Add(this.comboBoxProfile);
+            this.groupBox4.Location = new System.Drawing.Point(330, 15);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(278, 307);
+            this.groupBox4.TabIndex = 15;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Profile";
+            // 
+            // comboBoxProfile
+            // 
+            this.comboBoxProfile.FormattingEnabled = true;
+            this.comboBoxProfile.Items.AddRange(new object[] {
+            "Testa",
+            "Base",
+            "Distanziale",
+            "Anelli SA (Self-Aligning)"});
+            this.comboBoxProfile.Location = new System.Drawing.Point(29, 40);
+            this.comboBoxProfile.Name = "comboBoxProfile";
+            this.comboBoxProfile.Size = new System.Drawing.Size(224, 24);
+            this.comboBoxProfile.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::TestaUN.Properties.Resources.testa;
+            this.pictureBox1.InitialImage = global::TestaUN.Properties.Resources.testa;
+            this.pictureBox1.Location = new System.Drawing.Point(29, 90);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(224, 196);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button_insert
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(626, 403);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.labelWarning);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "button_insert";
+            this.Text = "Disegnatrice DEMO";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton radioButton_endless;
+        private System.Windows.Forms.RadioButton radioButton_split;
+        private System.Windows.Forms.TextBox textBox_intDia;
+        private System.Windows.Forms.TextBox textBox_extDia;
+        private System.Windows.Forms.TextBox textBox_height;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.RadioButton radioButton_nylon;
+        private System.Windows.Forms.RadioButton radioButton_ptfe;
+        private System.Windows.Forms.Label labelWarning;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.ComboBox comboBoxProfile;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
